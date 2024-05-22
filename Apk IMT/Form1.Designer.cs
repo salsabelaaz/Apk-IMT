aab22faed3c89d513456edc1990858829a9f0eca
SALSABILA <SALSABILA@LAPTOP-TGJAMJF4>
2024-05-22 14:22:46 +0700

    Add project files.

---
+++ Apk IMT/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Apk_IMT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHitung = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtUsia = new System.Windows.Forms.TextBox();
+            this.txtBeratbadan = new System.Windows.Forms.TextBox();
+            this.txtTinggibadan = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHitung
+            // 
+            this.btnHitung.BackColor = System.Drawing.Color.Silver;
+            this.btnHitung.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHitung.Location = new System.Drawing.Point(38, 371);
+            this.btnHitung.Name = "btnHitung";
+            this.btnHitung.Size = new System.Drawing.Size(126, 45);
+            this.btnHitung.TabIndex = 0;
+            this.btnHitung.Text = "Proses";
+            this.btnHitung.UseVisualStyleBackColor = false;
+            this.btnHitung.Click += new System.EventHandler(this.btnHitung_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Wanita",
+            "Pria"});
+            this.comboBox1.Location = new System.Drawing.Point(170, 222);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(176, 28);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(167, 117);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(233, 26);
+            this.txtNama.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("High Tower Text", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(243, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Indek Masa Tubuh";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Silver;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(185, 371);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(127, 45);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click_1);
+            // 
+            // txtUsia
+            // 
+            this.txtUsia.Location = new System.Drawing.Point(167, 176);
+            this.txtUsia.Name = "txtUsia";
+            this.txtUsia.Size = new System.Drawing.Size(233, 26);
+            this.txtUsia.TabIndex = 5;
+            // 
+            // txtBeratbadan
+            // 
+            this.txtBeratbadan.Location = new System.Drawing.Point(567, 115);
+            this.txtBeratbadan.Name = "txtBeratbadan";
+            this.txtBeratbadan.Size = new System.Drawing.Size(195, 26);
+            this.txtBeratbadan.TabIndex = 6;
+            // 
+            // txtTinggibadan
+            // 
+            this.txtTinggibadan.Location = new System.Drawing.Point(567, 175);
+            this.txtTinggibadan.Name = "txtTinggibadan";
+            this.txtTinggibadan.Size = new System.Drawing.Size(195, 26);
+            this.txtTinggibadan.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Usia";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(33, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Jenis kelamin";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(419, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Berat badan";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(419, 176);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Tinggi badan";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTinggibadan);
+            this.Controls.Add(this.txtBeratbadan);
+            this.Controls.Add(this.txtUsia);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnHitung);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHitung;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtUsia;
+        private System.Windows.Forms.TextBox txtBeratbadan;
+        private System.Windows.Forms.TextBox txtTinggibadan;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Apk IMT/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Apk_IMT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHitung_Click(object sender, EventArgs e)
+        {
+            double tinggi, berat, imt;
+            string ket1, ket2, ket3;
+            string nama = txtNama.Text;
+            ket1 = "Berat badan anda normal";
+            ket2 = "Underweight, anda perlu menaikkan berat badan anda";
+            ket3 = "Overweight, anda perlu menurunkan berat badan anda";
+            tinggi = Convert.ToDouble(txtTinggibadan.Text) / 100;
+            berat = Convert.ToDouble(txtBeratbadan.Text);
+            imt = berat / (tinggi * tinggi);
+            if (imt >= 18 && imt <= 21)
+            {
+                MessageBox.Show("IMT anda adalah " + nama + " " + imt + ket1);
+            }
+            else if (imt <= 18)
+            {
+                MessageBox.Show("IMT anda adalah " + nama + " " + imt + ket2);
+            }
+            else
+            {
+                MessageBox.Show("IMT anda adalah " + nama + " " + imt + ket3);
+            }
+        }
+
+        private void btnReset_Click_1(object sender, EventArgs e)
+        {
+            txtNama.Text = " ";
+            txtUsia.Text = " ";
+            txtBeratbadan.Text = " ";
+            txtTinggibadan.Text = " ";
+            comboBox1.Text = " ";
+        }
+    }
+}
+        
+    
+
